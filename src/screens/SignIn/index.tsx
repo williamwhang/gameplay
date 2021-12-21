f0fba712fd17f6d3248235edf4314ec23ba03a19
williamwhang <whang.william@gmail.com>
2021-12-21 13:42:27 -0300

    Splash Screen, SignIn Screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,30 +1,46 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
     View,
     Text,
     Image,
+    StatusBar
 } from 'react-native';
 
+import { ButtonIcon } from '../../components/ButtonIcon';
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
 export function SignIn() {
-    const [text, setText] = useState('');
-
     return (
         <View style={styles.container}>
-
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
             <Image
                 source={IllustrationImg}
                 style={styles.image}
+                resizeMode="stretch"
             />
 
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Organize
-                    suas jogatinas
-                    facilmente
+                    Organize {`\n`}
+                    suas jogatinas {`\n`}
+                    facilmente {`\n`}
                 </Text>
+
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {`\n`}
+                    favoritos com seus amigos
+                </Text>
+
+
+                <ButtonIcon
+                    title="Entrar com Discord"
+                    activeOpacity={0.7}
+                />
 
             </View>
         </View>
